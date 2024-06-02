392aec2dda086a597e0ca1585d980507394a7fc4
Loca94 <davide.loca94@gmail.com>
2024-06-02 22:46:40 +0200

    Refactor AnimatedBadgeList component input variable name

--- src/components/animations/AnimatedBadgeList.tsx
+++ src/components/animations/AnimatedBadgeList.tsx
@@ -3,22 +3,22 @@
 import { useEffect, useState } from 'react';
 import { BadgeList, BadgeListItem } from '@/components/ui/Badge';
 
-export function AnimatedBadgeList({ content }: { content: string[] }) {
+export function AnimatedBadgeList({ badges }: { badges: string[] }) {
   const [activeBadgeIndex, setActiveBadgeIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       // Increment the active badge index
-      setActiveBadgeIndex((prevIndex) => (prevIndex + 1) % content.length);
+      setActiveBadgeIndex((prevIndex) => (prevIndex + 1) % badges.length);
     }, 1500); // Change the duration as needed
 
     // Cleanup function to clear the interval
     return () => clearInterval(interval);
-  }, [content.length]);
+  }, [badges.length]);
 
   return (
     <BadgeList>
-      {content.map((text: string, index: number) => (
+      {badges.map((text: string, index: number) => (
         <BadgeListItem
           key={index}
           variant={index === activeBadgeIndex ? 'default' : 'disabled'}
