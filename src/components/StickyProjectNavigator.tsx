f376d67d1093eccdc51b6710d957c5aeefda444c
Loca94 <davide.loca94@gmail.com>
2024-07-07 13:17:30 +0200

    Optimized redirects in StickyProjectNavigator component

--- src/components/StickyProjectNavigator.tsx
+++ src/components/StickyProjectNavigator.tsx
@@ -3,6 +3,7 @@ import {
   ChevronRightIcon,
   ExternalLinkIcon,
 } from '@/components/Icons';
+import Link from 'next/link';
 
 export default function StickyProjectNavigator({
   projectUrl,
@@ -18,27 +19,29 @@ export default function StickyProjectNavigator({
       <div className="flex w-full justify-center">
         <div className="relative w-full border-b-2 border-slate-900 bg-slate-950 sm:w-fit sm:rounded sm:border-2">
           <div className="flex justify-center text-sm text-slate-400">
-            <a
+            <Link
               href={prevUrl}
               className="flex items-center px-4 py-3 font-semibold transition-colors hover:bg-slate-900 focus:bg-slate-900"
             >
               <ChevronLeftIcon className="mr-1.5 h-4 w-4" />
               Prev
-            </a>
+            </Link>
             <a
               href={projectUrl}
+              target="_blank"
+              rel="noopener noreferrer"
               className="flex items-center justify-center px-4 py-3 font-semibold transition-colors hover:bg-slate-900 focus:bg-slate-900"
             >
               <ExternalLinkIcon className="mr-1.5 h-4 w-4" />
               View website
             </a>
-            <a
+            <Link
               href={nextUrl}
               className="flex items-center px-4 py-3 font-semibold transition-colors hover:bg-slate-900 focus:bg-slate-900"
             >
               Next
               <ChevronRightIcon className="ml-1.5 h-4 w-4" />
-            </a>
+            </Link>
           </div>
         </div>
       </div>
